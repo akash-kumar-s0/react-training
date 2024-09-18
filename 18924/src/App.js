dc4d5c8a997403fa3df13914f1f05dc132527b6b
akashkumars <akashkumars@presidio.com>
2024-09-19 00:18:32 +0530

    use fetch

--- 18924/src/App.js
+++ 18924/src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 import { ShowList } from './components/list-component/ShowList';
-import data from './task-data';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [data, setData] = useState()
+
+  const fetchData = async() => {
+    let data =  await fetch('task-data.json')
+    setData(await data.json())
+  }
+  console.log("render");
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
+
   return (
     <>
-    <div>
-        <h1 className='m-2 '>Total : {data.total}</h1>
-      </div>
-       <div className='grid grid-cols-4 grid-rows-4 gap-2 m-2'><ShowList data = {data}/></div>
-    
+      { data ? (
+      <div>
+       <div>
+          <h1 className='m-2 '>Total : {data.total}</h1>
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 m-2'><ShowList data = {data}/></div>
+        </div>):
+        "Loading"
+      }
     </>
   )
 }
