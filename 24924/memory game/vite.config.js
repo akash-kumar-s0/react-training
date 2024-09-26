fa404c85764b6cf4292821e8bef40f7c02b9c52b
akashkumars <akashkumars@presidio.com>
2024-09-26 14:11:03 +0530

    build added

--- 24924/memory game/vite.config.js
+++ 24924/memory game/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/akash-kumar-s0/react-training/24924/memory%20game"
+  base:"/react-training/24924/memory%20game"
 })
