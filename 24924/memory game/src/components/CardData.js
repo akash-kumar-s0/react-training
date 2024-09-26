719c544ccccc6c10ccc330022e6af9f2c3602480
akashkumars <akashkumars@presidio.com>
2024-09-26 16:10:37 +0530

    final code

--- 24924/memory game/src/components/CardData.js
+++ 24924/memory game/src/components/CardData.js
@@ -1,52 +1,50 @@
 const animals = [
-  "Lion",
-  "Tiger",
-  "Bear",
-  "Wolf",
-  "Fox",
-  "Deer",
-  "Elephant",
-  "Giraffe",
-  "Zebra",
-  "Hippo",
-  "Rhino",
-  "Kangaroo",
-  "Koala",
-  "Panda",
-  "Penguin",
-  "Owl",
-  "Eagle",
-  "Hawk",
-  "Parrot",
-  "Toucan",
-  "Crocodile",
-  "Turtle",
-  "Frog",
-  "Snake",
-  "Shark",
-  "Dolphin",
-  "Whale",
-  "Octopus",
-  "Crab",
-  "Lobster",
-  "Butterfly",
-  "Bee",
-  "Ant",
-  "Spider",
-  "Scorpion",
-  "Camel",
-  "Llama",
-  "Gorilla",
-  "Chimpanzee",
-  "Sloth",
-  "Armadillo",
-  "Platypus",
-  "Hedgehog",
-  "Squirrel",
-  "Raccoon",
-  "Badger",
-  "Otter",
-  "Meerkat",
+  "🦁", // Lion
+  "🐯", // Tiger
+  "🐻", // Bear
+  "🐺", // Wolf
+  "🦊", // Fox
+  "🦌", // Deer
+  "🐘", // Elephant
+  "🦒", // Giraffe
+  "🦓", // Zebra
+  "🦛", // Hippo
+  "🦏", // Rhino
+  "🦘", // Kangaroo
+  "🐨", // Koala
+  "🐼", // Panda
+  "🐧", // Penguin
+  "🦉", // Owl
+  "🦅", // Eagle
+  "🦜", // Parrot
+  "🦤", // Toucan (no specific emoji for toucan, but this is the closest)
+  "🐊", // Crocodile
+  "🐢", // Turtle
+  "🐸", // Frog
+  "🐍", // Snake
+  "🦈", // Shark
+  "🐬", // Dolphin
+  "🐋", // Whale
+  "🐙", // Octopus
+  "🦀", // Crab
+  "🦞", // Lobster
+  "🦋", // Butterfly
+  "🐝", // Bee
+  "🐜", // Ant
+  "🕷️", // Spider
+  "🦂", // Scorpion
+  "🐫", // Camel
+  "🦙", // Llama
+  "🦍", // Gorilla
+  "🦧", // Chimpanzee (closest emoji is Orangutan)
+  "🦥", // Sloth
+  "🦣", // Armadillo (no emoji, closest is mammoth)
+  "🦆", // Platypus (no emoji, but duck is a close replacement)
+  "🦔", // Hedgehog
+  "🐿️", // Squirrel
+  "🦝", // Raccoon
+  "🦡", // Badger
+  "🦦", // Otter
 ];
 
 export default animals;
--- 24924/memory game/src/components/ui/Card.jsx
+++ 24924/memory game/src/components/ui/Card.jsx
@@ -1,16 +1,26 @@
+import React from "react";
+import "./Card.css"; 
+
 const Card = ({ id, animal, isFlipped, isSolved, onClick }) => (
   <div
-    className={`h-24 flex items-center justify-center cursor-pointer ${
-      isFlipped || isSolved ? "bg-blue-200" : "bg-black"
-    }`}
+    className={`h-36 flex rounded-md items-center justify-center cursor-pointer perspective`}
     onClick={() => onClick(id)}
   >
     <div
-      className={`text-center ${
-        isFlipped || isSolved ? "text-black" : "text-white"
-      } `}
+      className={`relative w-full rounded-md h-full transform-style-preserve-3d transition-transform duration-500 ease-in-out ${
+        isFlipped || isSolved ? "rotate-y-180" : ""
+      }`}
     >
-      {isFlipped || isSolved ? animal : "?"}
+      <div
+        className={`absolute w-full rounded-md h-full backface-hidden bg-black flex items-center justify-center shadow-lg text-center text-5xl text-white`}
+      >
+        ?
+      </div>
+      <div
+        className={`absolute w-full rounded-md h-full backface-hidden bg-slate-300 flex items-center justify-center shadow-lg text-center text-8xl text-black rotate-y-180`}
+      >
+        {animal}
+      </div>
     </div>
   </div>
 );
